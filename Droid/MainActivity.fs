c556916d4feaedf11da6bf1a08f6aefce2fa07d0
Sam Williams <willsam100@gmail.com>
2016-07-19 11:41:16 -0700

    Initinal commit

---
+++ Droid/MainActivity.fs
@@ -0,0 +1,20 @@
+﻿namespace SimpleList.Droid
+open System;
+
+open Android.App;
+open Android.Content;
+open Android.Content.PM;
+open Android.Runtime;
+open Android.Views;
+open Android.Widget;
+open Android.OS;
+
+[<Activity (Label = "SimpleList.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = (ConfigChanges.ScreenSize ||| ConfigChanges.Orientation))>]
+type MainActivity() =
+    inherit Xamarin.Forms.Platform.Android.FormsApplicationActivity()
+    override this.OnCreate (bundle: Bundle) =
+        base.OnCreate (bundle)
+
+        Xamarin.Forms.Forms.Init (this, bundle)
+        this.LoadApplication (new SimpleList.App ())
+
